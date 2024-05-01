1b3ae928a8b41cb4025fd516189478fd2b4c5417
alex-style1007 <68159727+alex-style1007@users.noreply.github.com>
2024-05-01 12:17:23 -0500

    .env y data usada en formato mongo, al igual que modificacion de la conexion

--- project/app/context/db_connection.py
+++ project/app/context/db_connection.py
@@ -1,13 +1,7 @@
-from sqlalchemy import create_engine, MetaData
-from sqlalchemy.engine import URL
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.pool import NullPool
 import os
-
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://adminReactor:Topicos@cluster0.3l35xwi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
