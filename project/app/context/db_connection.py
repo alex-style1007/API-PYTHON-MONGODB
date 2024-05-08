c917138566755e9c347b03807aacd84c87e5c958
Agudelo <Mariana.Agudelo@btgpactual.com>
2024-05-08 17:09:07 -0500

    ultimos ajustes en la conexión

--- project/app/context/db_connection.py
+++ project/app/context/db_connection.py
@@ -6,11 +6,7 @@ class MongoDB():
     client = None
 
     def __init__(self):
-        user = "API" #ELIMINAR
-        password = "4321" #ELIMINAR
-        cluster_info = "cluster0.3l35xwi.mongodb.net" #ELIMINAR
-        # uri = os.environ["uri"] #DESCOMENTAR
-        uri = f"mongodb+srv://{user}:{password}@{cluster_info}/?retryWrites=true&w=majority" #ELIMINAR
+        uri = os.environ["uri"] 
         self.client = MongoClient(uri, connectTimeoutMS=60000)
 
     def _get_collection(self, dbname: str, collection: str):
